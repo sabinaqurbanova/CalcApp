e274b54d45920be12c2d3aecbd335e3ffc03987a
Sabina <qurbanowsebine@gmail.com>
2023-11-09 19:35:23 +0400

    proqramin ilkin final versiyasi hazirdir

---
+++ CalcApp/CalcApp/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace CalcApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Variables
+            double num1;
+            double num2;
+            double result;
+            char operation = '+';
+            #endregion
+            char again = 'y';
+
+            do
+            {
+                Console.WriteLine("---------------------------------------------------------------------------------------------------------------");
+                Console.Write("First Number: ");
+                num1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Choose your operation: ");
+                operation = Convert.ToChar(Console.ReadLine());
+
+                Console.Write("Second Number: ");
+                num2 = Convert.ToDouble(Console.ReadLine());
+
+                if (operation == '+')
+                {
+                    result = num1 + num2;
+                    Console.WriteLine("The result is: " + result);
+                }
+                else if (operation == '-')
+                {
+                    result = num1 - num2;
+                    Console.WriteLine("The result is: " + result);
+                }
+                else if (operation == '*')
+                {
+                    result = num1 * num2;
+                    Console.WriteLine("The result is: " + result);
+                }
+                else if (operation == '/')
+                {
+                    result = num1 / num2;
+                    Console.WriteLine("The result is: " + result);
+                }
+                else Console.WriteLine("The operation is wrong");
+
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("Calculate Again? (y/n)");
+                again = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine("---------------------------------------------------------------------------------------------------------------");
+
+            } while (again == 'y');
+        }
+    }
+}
